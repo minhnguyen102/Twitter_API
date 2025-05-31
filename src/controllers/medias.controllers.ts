a5fe7dd0263c37a142c87929029c4d879bee141a
Minh Nguyen <minhkhac1002@gmail.com>
2025-05-31 14:57:36 +0700

    Filter upload image

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,22 +1,11 @@
 import { NextFunction, Request, Response } from "express"
 import path from "path";
+import { handlerUploadSingleImage } from "~/utils/file";
 
 // [POST] /medias/uploadSingleFile
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const formidable = (await (import('formidable'))).default // Cách import 1 file esmodule vào dự án common js
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    keepExtensions: true,
-    maxFiles: 1,
-    maxFileSize: 3000 * 1024 * 1024 // 300MB
-  });
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      message: "Upload file successfully",
-      fields, files 
-    });
-  });
+  const data = await handlerUploadSingleImage(req as any)
+  res.json({
+    data: data
+  })
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,5 +1,7 @@
+import { error } from 'console'
 import fs from 'fs'
-import path from 'path'
+import path, { resolve } from 'path'
+
 export const initFolder = () =>{
   const uploadFolderPath = path.resolve('uploads')
   if(!fs.existsSync(uploadFolderPath)){
@@ -8,4 +10,35 @@ export const initFolder = () =>{
       recursive: true // cho phép nested
     })
   }
+}
+
+export const handlerUploadSingleImage = async (req: Request) => {
+  const formidable = (await (import('formidable'))).default // Cách import 1 file esmodule vào dự án common js
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    keepExtensions: true,
+    maxFiles: 1,
+    maxFileSize: 300 * 1024 * 1024, // 300MB,
+    filter: function({name, originalFilename, mimetype}){
+      const valid = name ==="image" && Boolean(mimetype?.includes('image/'))
+      if(!valid){
+        form.emit('error' as any, new Error('Invalid type') as any)
+      }
+      return valid
+    }
+  });
+  return new Promise((resolve, reject) => {
+    form.parse(req as any, (err, fields, files) => {
+      // console.log("files", files)
+      // console.log("fields", fields)
+      // console.log('error:', Boolean(err))
+      if (err) {
+        return reject(err)
+      }
+      if(!Boolean(files.image)){
+        return reject(new Error("File is empty"))
+      }
+      resolve(files)
+    });
+  })
 }
