fbf0e81ed1c003e5c0048598463053c604a62548
Minh Nguyen <minhkhac1002@gmail.com>
2025-06-03 10:15:55 +0700

    static file

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/errors.middleware'
 import { initFolder } from './utils/file'
 import {config} from 'dotenv'
+import path from 'path'
+import { UPLOAD_DIR } from './constants/dir'
 
 
 config();
@@ -15,11 +17,11 @@ const port = process.env.PORT || 4000
 databaseService.run().catch(console.dir);
 app.use(express.json());
 app.use(router);
+app.use('/uploads', express.static(UPLOAD_DIR))
 
 app.use(defaultErrorHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
 
